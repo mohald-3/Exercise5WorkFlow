546b46898ec08e482eef26ea4353ab4406752f6d
Mohanad <mohanad.aldaghestani@gmail.com>
2025-11-14 12:37:30 +0100

    Added an math controller with addition and its test

---
+++ API/Controllers/MathController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MathController : ControllerBase
+    {
+        // GET /api/math/add?x=2&y=3
+        [HttpGet("add")]
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ Test/MathControllerTests.cs
@@ -0,0 +1,25 @@
+using API.Controllers;
+
+namespace Test
+{
+    public class Tests
+    {
+        public class MathControllerTests
+        {
+            [Test]
+            public void Add_ReturnsCorrectSum()
+            {
+                // Arrange
+                var controller = new MathController();
+                int x = 2;
+                int y = 3;
+
+                // Act
+                int result = controller.Add(x, y);
+
+                // Assert
+                Assert.That(result, Is.EqualTo(5));
+            }
+        }
+    }
+}
