7ce47eb3c426d737b044d081f9c6e3960ee735b9
Mohanad Al-Daghestani <92151588+mohald-3@users.noreply.github.com>
2025-11-20 18:18:26 +0100

    Update MathController.cs

--- API/Controllers/MathController.cs
+++ API/Controllers/MathController.cs
@@ -7,7 +7,8 @@ namespace API.Controllers
     [ApiController]
     public class MathController : ControllerBase
     {
-        // Controller
+        // Controller 
+        // adding this text to see if the auto-labeler works
         // GET /api/math/add?x=2&y=3
         [HttpGet("add")]
         public int Add(int x, int y)
